fdb506a155294155f4358016b66f85232b10a3cf
Juhgabrielly <juliagonzaga062@gmail.com>
2025-06-05 14:10:43 -0300

    cinema.c

--- cinema.c
+++ cinema.c
@@ -2,17 +2,23 @@
 
 int main() {
     int ingressos, pontos, continuar;
+    float precoingre = 25.00;
+    float valor;
 
-    do {
+    do{
         printf("Bem vindo ao Cinema\n");
         printf("Digite a quantidade de ingressos comprados: ");
         scanf("%d", &ingressos);
 
-        pontos = ingressos;
+        pontos = ingressos; 
+        valor = ingressos * precoingre; 
+
         printf("Cliente acumulou %d pontos.\n", pontos);
+        printf("Valor total da compra: R$ %.2f\n", valor);
 
         printf("Deseja cadastrar outro cliente? (1-Sim / 0-Não): ");
         scanf("%d", &continuar);
+        
     } while (continuar != 0);
 
     printf("Sistema encerrado.\n");
